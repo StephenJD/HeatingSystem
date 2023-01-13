7a0aa518f1ffa1cd6f031b3b69a51bd0fdb6f53b
Stephen Dolley <sjdolley@live.co.uk>
2023-01-13 12:49:44 +0000

    .begin only before WaitToFinish fixes it.

--- I2C_Talk/I2C_To_MicroController.cpp
+++ I2C_Talk/I2C_To_MicroController.cpp
@@ -189,7 +189,7 @@ namespace HardwareInterfaces {
 			logger() << L_time << F("send_data 0x") << L_hex << getAddress() << F(" Reg 0x") << _remoteRegOffset + remoteRegNo << F(" read: ") << read_data << L_endl;
 			if ((read_data & HANDSHAKE_MASK) == DATA_READ) break;
 			writeOnly_RegValue(remoteRegNo, SEND_DATA);
-			i2C().begin();
+			//i2C().begin();
 		} while (!timeout);
 		auto timeused = timeout.timeUsed();
 		//if (timeused > 200 && !timeout) 
@@ -228,6 +228,7 @@ namespace HardwareInterfaces {
 
 		const uint8_t COMPLETE_DATA = localReg.get(regNo) | EXCHANGE_COMPLETE;
 		const uint8_t RECEIVE_OK = localReg.get(regNo) | DATA_READ & ~DATA_SENT;
+		i2C().begin();
 
 		bool hasFinished = true;
 		if ((localReg.get(regNo) & DATA_MASK) == DEVICE_CAN_WRITE) {
